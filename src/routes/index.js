bd6be5105665431f204ca4889b3f6ddb8c42f924
heeyonhh <lightram23@gmail.com>
2023-09-08 15:12:21 +0900

    deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,8 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter } from "vue-router";
 import Forecast from "~/components/Forecast";
 import Airquality from "~/components/Airquality";
 
 export default createRouter({
-
-    //hash모드 : 특정 페이지에서 새로고침했을때, 페이지 찾을수 없음과 같은 메세지 알림을 방지
-    history: createWebHashHistory(),
 
     routes: [
         {
