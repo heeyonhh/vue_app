cd6f457702d2697ef6b02b0481662a55dea7dafc
heeyonhh <lightram23@gmail.com>
2023-09-08 16:54:40 +0900

    deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,25 +2,21 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Forecast from "~/components/Forecast";
 import Airquality from "~/components/Airquality";
 
-const routes = [
-    {
-        path: "/",
-        name: "Forecast",
-        component: Forecast,
-    },
-    {
-        path: "/airquality",
-        name: "Airquality",
-        component: Airquality,
-    },
-    {
-        path: '/:pathMatch(.*)*',
-        redirect: "/404"
-    },
-];
-
 export default createRouter({
-    // hash 모드: 특정 페이지에서 새로고침했을 때, 페이지 찾을 수 없음과 같은 메시지 알림을 방지
+
+    //hash모드 : 특정 페이지에서 새로고침했을때, 페이지 찾을수 없음과 같은 메세지 알림을 방지
     history: createWebHashHistory(),
-    routes,
-});
+
+    routes: [
+        {
+            path: "/",
+            name: Forecast,
+            component: Forecast,
+        },
+        {
+            path: "/airquality",
+            name: Airquality,
+            component: Airquality,
+        },
+    ]
+})
